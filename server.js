7eb6fe37830f019c8df6fabb3653aaae4ad68968
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-18 14:44:29 -0800

    added error page

--- server.js
+++ server.js
@@ -47,11 +47,12 @@ function newSearch(req, res) {
     .then(data => {
       const ticketMasterEvent = data.body._embedded.events.map( events => {
         return new TicketMaster(events);
-      });
+      })
+        .then(eventsArr => res.render('pages/searches/events', { eventsArrKey: eventsArr }))
       res.status(200).json(ticketMasterEvent);
     })
     .catch(() => {
-      errorHandler(`So sorry, something went wrong`, req, res);
+      res.render('pages/error');
     });
 }
 
@@ -62,7 +63,7 @@ function TicketMaster(events) {
   this.description = events.info;
   this.address_line_1 = events._embedded.venues[0].address.line1;
   this.address_line_2 = events._embedded.venues[0].address.line2;
-  this.address_line_3 = `${events._embedded.venues[0].city.name}, ${events._embedded.venues[0].state.stateCode} ${events._embedded.venues[0].postalCode}`;
+  this.address_line_2= `${events._embedded.venues[0].city.name}, ${events._embedded.venues[0].state.stateCode} ${events._embedded.venues[0].postalCode}`;
   this.img_url = events.images[0].url;
   console.log('venues at 0: ', events._embedded.venues[0]);
 }
